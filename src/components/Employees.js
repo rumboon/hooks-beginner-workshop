2bcb56ae1c8f1f25b9cfedd6f149b2d52b98b087
Paul Boelens <paulboelens@saxum.nl>
2019-09-11 18:13:00 +0200

    Employees to class-based

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -1,32 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Employee from './Employee';
 import { searchFilter } from '../utils/employees';
 import { List, ListItem } from '../styles/Employees';
-import useFetch from '../hooks/useFetch';
 
-const Employees = ({ searchValue, filters }) => {
-    const [filteredEmployees, setFilteredEmployees] = React.useState();
-    const employees = useFetch('http://localhost:5000/api/employees');
+class Employees extends Component {
+    state = {
+        filteredEmployees: undefined,
+        employees: [],
+    }
 
-    React.useEffect(() => {
-        if (!employees || !employees.length) return;
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    componentDidUpdate(prevProps) {
+        const { searchValue, filters} = this.props;
+
+        if (searchValue !== prevProps.searchValue || filters !== prevProps.filters) {
+            this.setFilteredEmployees();
+        }
+    }
+
+    fetchData = async () => {
+        let result = [];
+
+        try {
+            const response = await fetch('http://localhost:5000/api/employees');
+            result = await response.json();
+        } catch(e) {
+            console.error(e);
+        }
+
+        this.setState({
+            employees: result
+        });
+    }
+
+    setFilteredEmployees = () => {
+        const { employees } = this.state;
+    
+        const filteredData = employees
+            .filter(employee => this.props.filters.includes(employee.function))
+            .filter(searchFilter(this.props.searchValue));
         
-        setFilteredEmployees(employees
-            .filter(employee => filters.includes(employee.function))
-            .filter(searchFilter(searchValue))
+        if (filteredData.length > 0) {
+            this.setState({
+                filteredEmployees: filteredData
+            });
+        }
+    }
+
+    render() {
+        const { filteredEmployees, employees } = this.state;
+
+        return (
+            <List>
+                {(filteredEmployees || employees).map((employee, i) => (
+                    <ListItem key={employee.id} isOdd={Boolean(i % 2)}>
+                        <Employee {...employee} />
+                    </ListItem>
+                ))}
+            </List>
         );
-
-    }, [searchValue, filters, employees]);
-
-    return (
-        <List>
-            {(filteredEmployees || employees).map((employee, i) => (
-                <ListItem key={employee.id} isOdd={Boolean(i % 2)}>
-                    <Employee {...employee} />
-                </ListItem>
-            ))}
-        </List>
-    );
+    }
+    
 }
 
 export default Employees;
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,6 @@ import { Input } from '../styles/Search';
 
 const Search = (props) => {
     const handleInput = (e) => {
-        // NOTE could createa an assignment for creating a debounce custom hook
         props.onSearch(e.currentTarget.value);
     }
 
